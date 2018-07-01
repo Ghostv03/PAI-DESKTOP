daba169b5699524d7aaf7e5b156cda3755013c7e
Ghostv03 <40476648+Ghostv03@users.noreply.github.com>
2018-07-01 15:19:20 +0200

    Apka destopowa

---
+++ PAI-DESKTOP/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PAI_DESKTOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var menu = new MenuWindow(); 
+            menu.Show(); 
+            this.Close(); 
+         
+        }
+    }
+}
---
+++ PAI-DESKTOP/MenuWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PAI_DESKTOP
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Zamowienia_Click(object sender, RoutedEventArgs e)
+        {
+            zamowieniaGrid.Visibility = Visibility.Collapsed;
+            productsGrid.Visibility = Visibility.Collapsed;
+            usersGrid.Visibility = Visibility.Collapsed;
+
+            zamowieniaGrid1.Visibility = Visibility.Visible;
+            zamowieniaGrid2.Visibility = Visibility.Visible;
+            zamowieniaGrid3.Visibility = Visibility.Visible;
+        }
+
+        private void Produkty_Click(object sender, RoutedEventArgs e)
+        {
+            zamowieniaGrid.Visibility = Visibility.Collapsed;
+            productsGrid.Visibility = Visibility.Collapsed;
+            usersGrid.Visibility = Visibility.Collapsed;
+
+            productsGrid1.Visibility = Visibility.Visible;
+            productsGrid2.Visibility = Visibility.Visible;
+            productsGrid3.Visibility = Visibility.Visible;
+
+        }
+
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+            zamowieniaGrid.Visibility = Visibility.Collapsed;
+            productsGrid.Visibility = Visibility.Collapsed;
+            usersGrid.Visibility = Visibility.Collapsed;
+
+            usersGrid1.Visibility = Visibility.Visible;
+            usersGrid2.Visibility = Visibility.Visible;
+            usersGrid3.Visibility = Visibility.Visible;
+
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close(); 
+        }
+
+        private void End_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
